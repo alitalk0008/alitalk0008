424e24c9d85f2f2fb908313bcb0c4f36106cee4d
alitalk0008 <alitalk0008@gmail.com>
2025-12-10 16:38:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -559,7 +559,7 @@ async function fetchByCategory({ categoryId }) {
   console.log("dataBaseRes", listTasks.dataBaseRes.length);
   console.log("item", listTasks.item.length);
 
-  const items = (listTasks.item ?? []).filter((p) => Number(p?.volume) >= 170);
+  const items = (listTasks.item ?? []).filter((p) => Number(p?.volume) >= 100);
   const dbs = listTasks.dataBaseRes ?? [];
 
   const merged = [
@@ -1003,7 +1003,7 @@ async function fetchByCategory({ categoryId }) {
                 },
                 arrayFilters: [
                   {
-                    "e.sId": sId,
+                    // "e.sId": sId,
                     $and: [
                       {
                         $or: [
